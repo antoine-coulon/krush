a0352520a7d23c5160d37b1615642df49123559e
antoine-coulon <coulon_antoine@outlook.fr>
2023-01-26 20:58:49 +0100

    chore: remove boilerplate code

--- plugins/rush-plugin-skott/src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { doSomething } from "./index.js";
-
-describe("Something", () => {
-  test("should do something", () => {
-    expect(doSomething()).toBe("Hello World");
-  });
-});
