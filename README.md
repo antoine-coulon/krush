6c5b6d865c6d14ad90f1b4ff58d206f89055eff2
antoine-coulon <coulon_antoine@outlook.fr>
2023-02-01 17:44:58 +0100

    plugin-skott: enhance cli reporting for the unused command

--- README.md
+++ README.md
@@ -14,9 +14,11 @@ This command generates a graph of your Rush monorepo projects and opens a webui 
 
 ![skott-rush](https://user-images.githubusercontent.com/43391199/215892215-eee3b70a-4186-4669-b02c-18b0f376234e.png)
 
-#### `rush list-unused-packages` (WIP)
+#### `rush list-unused-packages`
 
 This command traverses each of the Rush monorepo projects and search for unused `npm` packages in production code.
+
+![rush-list-unused](https://user-images.githubusercontent.com/43391199/216106483-4d6be883-1d2d-4bb0-a1ca-bc2c04884af6.png)
 
 ## Install
 
--- plugins/rush-plugin-skott/src/plugins/unused/main.ts
+++ plugins/rush-plugin-skott/src/plugins/unused/main.ts
@@ -1,8 +1,10 @@
+import { performance } from "node:perf_hooks";
+
 import RushSdk from "@rushstack/rush-sdk";
+import { Effect, pipe } from "effect";
 import kleur from "kleur";
 import skott from "skott";
-
-import { Effect, pipe } from "effect";
+import { EcmaScriptDependencyResolver } from "skott/modules/resolvers/ecmascript/resolver";
 
 class RushConfigurationError extends Error {
   readonly _tag = "RushConfigurationError";
@@ -11,78 +13,112 @@ class RushConfigurationError extends Error {
   }
 }
 
-function loadRush() {
-  return Effect.tryCatch(
-    () => {
+function loadRushConfiguration() {
+  return pipe(
+    Effect.attempt(() => {
       const config = RushSdk.RushConfiguration.loadFromDefaultLocation({
         startingFolder: process.cwd(),
       });
 
       if (!config) {
         throw new Error();
-      } else {
-        console.log(
-          kleur.bold().green("✓"),
-          kleur.bold("Rush configuration found")
-        );
       }
 
-      return config;
-    },
-    () => {
-      return new RushConfigurationError(
-        "Could not load 'rush.json' configuration"
+      console.log(
+        kleur.bold().green("✓"),
+        kleur.bold().white("Rush configuration found")
       );
-    }
+
+      return config;
+    }),
+    Effect.orElseFail(
+      () =>
+        new RushConfigurationError("Could not load 'rush.json' configuration")
+    )
+  );
+}
+
+function collectUnusedDependencies({
+  path,
+  name,
+  relativePath,
+}: {
+  path: string;
+  name: string;
+  relativePath: string;
+}) {
+  return pipe(
+    Effect.tryPromise(() =>
+      skott({
+        cwd: path,
+        dependencyResolvers: [new EcmaScriptDependencyResolver()],
+        dependencyTracking: {
+          thirdParty: true,
+          typeOnly: false,
+          builtin: false,
+        },
+      }).then(({ findUnusedDependencies }) => findUnusedDependencies())
+    ),
+    Effect.map(({ thirdParty }) => ({
+      unused: thirdParty,
+      name,
+      relativePath,
+    })),
+    Effect.tapError(() => {
+      console.log();
+      console.log(
+        kleur.bold().yellow(`⚠ ${name} skipped.`),
+        kleur.bold().gray("Reason: invalid or missing package.json")
+      );
+      return Effect.unit();
+    })
   );
 }
 
 function main() {
   return pipe(
-    loadRush(),
+    loadRushConfiguration(),
     Effect.map((config) =>
-      config.projects.map(({ packageName, projectFolder }) => ({
-        name: packageName,
-        path: projectFolder,
-      }))
-    ),
-    Effect.map((projects) =>
-      projects.map(({ path, name }) =>
-        pipe(
-          Effect.tryPromise(() =>
-            skott({
-              cwd: path,
-            }).then(({ findUnusedDependencies }) => findUnusedDependencies())
-          ),
-          Effect.map(({ thirdParty }) => ({ unused: thirdParty, name })),
-          Effect.catchAll(() => {
-            console.log(
-              kleur
-                .bold()
-                .red(`✖ ${name} - error while collecting unused dependencies`)
-            );
-            return Effect.succeed({ unused: [], name });
-          })
-        )
+      config.projects.map(
+        ({ packageName, projectFolder, projectRelativeFolder }) => ({
+          name: packageName,
+          path: projectFolder,
+          relativePath: projectRelativeFolder,
+        })
       )
     ),
+    Effect.map((projects) => projects.map(collectUnusedDependencies)),
     Effect.flatMap((listOfProjectsToAnalyze) =>
       pipe(
-        Effect.collectAllPar(listOfProjectsToAnalyze),
+        Effect.collectAllSuccessesPar(listOfProjectsToAnalyze),
         Effect.withParallelism(10)
       )
     ),
-    Effect.map((results) => {
-      for (const result of results) {
+    Effect.map((projectsResults) => {
+      for (const { name, unused, relativePath } of projectsResults) {
         console.log();
-        console.log(kleur.bold().magenta(result.name));
+        console.log(
+          kleur.bold().magenta(name),
+          kleur.bold().grey(`(${relativePath})`)
+        );
 
-        for (const unused of result.unused) {
-          console.log(" - ", kleur.bold().red(unused));
+        if (unused.length === 0) {
+          console.log(kleur.bold().green(" ✓ No unused dependencies"));
+          continue;
+        }
+
+        for (const packageName of unused) {
+          console.log(kleur.bold().yellow(` ✖ ${packageName}`));
         }
       }
     })
   );
 }
 
-Effect.unsafeRunPromise(main());
+const timing = performance.now();
+Effect.unsafeRunPromise(main()).finally(() => {
+  console.log();
+  console.log(
+    kleur.bold().white(`✨ Done in ${Math.round(performance.now() - timing)}ms`)
+  );
+});
